35158fe47213d4a89656fed6c7cd6d28100c3a14
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-04-23 17:49:26 +0000

    lab.c outlined

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,33 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+
+struct queue_t {
+    
+};
+
+queue_t queue_init(int capacity) {
+
+}
+
+void queue_destory(queue_t q) {
+
+}
+
+void enqueue(queue_t q, void *data) {
+
+}
+
+void *dequeue(queue_t q) {
+    return NULL;
+}
+
+void queue_shutdown(queue_t q) {
+
+}
+
+bool is_empty(queue_t q) {
+    return false;
+}
+
+bool is_shutdown(queue_t q) {
+    return false;
+}
