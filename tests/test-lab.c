649065462140ba9a9b48dfdf6cd016d541b16a34
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-04-29 23:15:30 +0000

    added tests

--- tests/test-lab.c
+++ tests/test-lab.c
@@ -73,11 +73,59 @@ void test_queue_dequeue_shutdown(void)
     queue_destroy(q);
 }
 
+/* START NEW TESTS */
+
+void test_is_empty(void)
+{
+    queue_t q = queue_init(10);
+    TEST_ASSERT_TRUE(q != NULL);
+    TEST_ASSERT_TRUE(is_empty(q));
+    int data = 1;
+    enqueue(q, &data);
+    TEST_ASSERT_TRUE(!is_empty(q));
+    dequeue(q);
+    TEST_ASSERT_TRUE(is_empty(q));
+    queue_destroy(q);
+}
+
+void test_is_shutdown(void)
+{
+  queue_t q = queue_init(10);
+  TEST_ASSERT_TRUE(q != NULL);
+  TEST_ASSERT_TRUE(!is_shutdown(q));
+  int data = 1;
+  enqueue(q, &data);
+  TEST_ASSERT_TRUE(!is_shutdown(q));
+  dequeue(q);
+  TEST_ASSERT_TRUE(!is_shutdown(q));
+  queue_shutdown(q);
+  TEST_ASSERT_TRUE(is_shutdown(q));
+  queue_destroy(q);
+}
+
+void test_null_safety(void)
+{
+  /* if these aren't null safe, they'll probably cause a segfault */
+  TEST_ASSERT_NULL(queue_init(NULL));
+  queue_destroy(NULL);
+  enqueue(NULL, NULL);
+  TEST_ASSERT_NULL(dequeue(NULL));
+  queue_shutdown(NULL);
+  TEST_ASSERT_TRUE(is_empty(NULL));
+  TEST_ASSERT_TRUE(is_shutdown(NULL));
+}
+
+/* END NEW TESTS */
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_create_destroy);
   RUN_TEST(test_queue_dequeue);
   RUN_TEST(test_queue_dequeue_multiple);
   RUN_TEST(test_queue_dequeue_shutdown);
+  printf("========== NEW TESTS ==========\n");
+  RUN_TEST(test_is_empty);
+  RUN_TEST(test_is_shutdown);
+  RUN_TEST(test_null_safety);
   return UNITY_END();
 }
